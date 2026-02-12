b473a3d4e0bf4dc81257c8d86afc90c73f81d1f9
milshael7 <wdelvoix23@gmail.com>
2026-02-12 02:14:39 -0600

    Update SecurityOverview.jsx

--- frontend/src/components/security/SecurityOverview.jsx
+++ frontend/src/components/security/SecurityOverview.jsx
@@ -1,1 +1,20 @@
+import React, { useRef } from "react";
+import SecurityRadar from "../components/security/SecurityRadar";
+import SecurityToolMarketplace from "../components/security/SecurityToolMarketplace";
 
+export default function SecurityOverview() {
+  const radarRef = useRef(null);
+
+  function refreshRadar() {
+    if (radarRef.current?.reload) {
+      radarRef.current.reload();
+    }
+  }
+
+  return (
+    <div className="postureWrap">
+      <SecurityRadar ref={radarRef} />
+      <SecurityToolMarketplace onChange={refreshRadar} />
+    </div>
+  );
+}
