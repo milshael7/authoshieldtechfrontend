cb82b10c3ea2dd739019c7fc052d0e3ab2f12abe
milshael7 <wdelvoix23@gmail.com>
2026-03-04 09:21:47 -0500

    Create ManagerCommand component for manager interface

---
+++ frontend/src/pages/manager/ManagerCommand.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function ManagerCommand() {
+
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>Manager Command Center</h2>
+
+      <p>
+        Operational command interface for managers.
+      </p>
+    </div>
+  );
+
+}
